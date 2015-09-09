b9a9f7c6fb1596097950bcd0c59f97a10c3e71df
Celia Chen <CoconutBoo@10.0.1.10>
2015-09-09 11:01:18 -0700

    IC-2 template code committed

---
+++ reverseString.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class reverseString{
+
+    public static void main(String[] args){
+
+	// Given an input of string
+	// reverse the input
+	// print out the original string
+	// and the reversed string
+
+	String input = "Everything is awesome!";
+
+    }
+
+}
