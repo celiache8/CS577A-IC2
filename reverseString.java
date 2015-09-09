dd58f1ba15edb0af1c93800b6b5cbc7ffe172b03
Celia Chen <CoconutBoo@10.0.1.10>
2015-09-09 11:11:30 -0700

    My version of reverse string

--- reverseString.java
+++ reverseString.java
@@ -12,6 +12,12 @@ public class reverseString{
 
 	String input = "Everything is awesome!";
 
+	StringBuilder input1 = new StringBuilder();
+	input1.append(input);
+	input1=input1.reverse(); 
+	for (int i=0;i<input1.length();i++)
+	    System.out.print(input1.charAt(i));  
+
     }
 
 }
