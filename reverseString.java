493f550b74bb5cdd0f5df124b29108ec34dac218
yunwei <yunweiyu@usc.edu>
2015-09-11 23:39:32 -0700

    simplify the code to output reverse string.

--- reverseString.java
+++ reverseString.java
@@ -11,13 +11,9 @@ public class reverseString{
 	// and the reversed string
 
 	String input = "Everything is awesome!";
-	StringBuffer sb = new StringBuffer();
-        for(int i = 0, j = input.length()-1; j>=0;j--){
-                sb.append(input.charAt(j));
-        }
-        System.out.println("Original String: "+input);
-        input = sb.toString();
-        System.out.println("Reversed String: "+input);	
+
+    System.out.println("Original String: "+input);
+    System.out.println("Reversed String: "+input.reverse());
     }
 
 }
